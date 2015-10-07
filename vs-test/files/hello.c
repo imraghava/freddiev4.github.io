0ba0a098fc2b25872b7b1b634607216f32d57ac2
Freddie <fv4.programmer@gmail.com>
2015-10-07 01:06:26 -0400

    Add hello.js

--- vs-test/files/hello.c
+++ vs-test/files/hello.c
@@ -5,7 +5,7 @@
  
 int main() {
 
-  printf("Hello world\n");
+  printf("Hello, World!\n");
   return 0;
-  
+
 }
---
+++ vs-test/files/hello.js
@@ -0,0 +1,8 @@
+// Author: FreddieV4
+// File: hello.js
+
+function hello(){
+
+	console.log("Hello, World!")
+
+}
